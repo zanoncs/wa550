8f2e6aa30e22a497dad6244588f8ef83fbe5c283
zanoncs <rodrigozanonzz@hotmail.com>
2025-02-18 01:03:16 -0300

    Delete frontend directory

--- frontend/src/utils/formatToCurrency.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable new-cap */
-export default function formatToCurrency(value) {
-  return Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(value);
-}
